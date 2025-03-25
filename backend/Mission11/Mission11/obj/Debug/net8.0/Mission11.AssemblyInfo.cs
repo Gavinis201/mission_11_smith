199272a01d652625f6b6c86e018de4fca90aba6d
Gavin Smith <gavinjacobsmith@gmail.com>
2025-03-24 22:50:45 -0600

    edited

--- backend/Mission11/Mission11/obj/Debug/net8.0/Mission11.AssemblyInfo.cs
+++ backend/Mission11/Mission11/obj/Debug/net8.0/Mission11.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission11")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e80edcde1cfca2fcd3014e1114d975bea5a5e59d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission11")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission11")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
